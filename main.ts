512226c0eb7492e70de065558981cb00075b17c7
humanapp <eric@humanappliance.com>
2020-11-29 21:17:36 -0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,8 @@
+const app = new kodu.App();
 
+//game.onUpdateInterval(5000, function () {
+    // Watch for sprite leaks.
+    // console.log("# sprites: " + game.currentScene().allSprites.length);
+    // Watch for memory leaks.
+    // control.heapSnapshot()
+//});
